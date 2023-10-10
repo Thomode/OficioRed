ad73450f341bf16843dcb0ef19e0f162a28c0f4c
Joaquin <89774@sistemas.frc.utn.edu.ar>
2023-10-10 10:49:42 -0300

    act buscador

--- OficioRed/ClientApp/src/components/buscador.jsx
+++ OficioRed/ClientApp/src/components/buscador.jsx
@@ -1,31 +1,31 @@
 import React from "react";
-import { TextField, Button, Grid } from "@mui/material";
+import { TextField, Button, Grid, Paper } from "@mui/material"; // Importamos Paper de MUI
 import SearchIcon from "@mui/icons-material/Search";
 
 function Buscador({ searchValue, setSearchValue, handleSearch }) {
-
     return (
-        <Grid container spacing={3} alignItems="center" justifyContent="space-between">
-            <Grid item xs={6}>
-                <TextField
-                    variant="outlined"
-                    placeholder="Buscar usuario"
-                    fullWidth
-                    size="small"
-                    value={searchValue}
+            <Grid container spacing={3} alignItems="center" justifyContent="left">
+                <Grid item xs={6}>
+                    <TextField
+                        variant="outlined"
+                        placeholder="Buscar usuario"
+                        fullWidth
+                        size="small"
+                        value={searchValue}
                     onChange={(e) => setSearchValue(e.target.value)}
-                />
+                    style={{ backgroundColor: "white" }} 
+                    />
+                </Grid>
+                <Grid item>
+                    <Button
+                        variant="contained"
+                        startIcon={<SearchIcon />}
+                        onClick={handleSearch}
+                    >
+                        Buscar
+                    </Button>
+                </Grid>
             </Grid>
-            <Grid item>
-                <Button
-                    variant="contained"
-                    startIcon={<SearchIcon />}
-                    onClick={handleSearch}
-                >
-                    Buscar
-                </Button>
-            </Grid>
-        </Grid>
     );
 }
 
