fe3c0098bb2cc2f333c41422134cd4df10445383
Tomas Figueroa Lopez <91357@sistemas.frc.utn.edu.ar>
2023-11-08 16:32:21 -0300

    dashboard

---
+++ OficioRed/Controllers/AdminController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OficioRed.Services;
+
+namespace OficioRed.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AdminController: ControllerBase
+{
+    private IUsuarioService _usuarioService;
+
+    public AdminController(IUsuarioService usuarioService)
+    {
+        _usuarioService = usuarioService;
+    }
+
+    [HttpGet("Dashboard")]
+    public IActionResult GetDashboard()
+    {
+        try
+        {
+            var usuarios = _usuarioService.GetAll();
+
+            int totalUsuario = 0;
+            int cantidadAdmin = 0;
+            int cantidadProfesional = 0;
+            int cantidadInteresado = 0;
+
+            foreach (var usuario in usuarios)
+            {
+                if (usuario.IdRol == 2)
+                {
+                    cantidadAdmin++;
+                }
+                else if (usuario.IdRol == 3)
+                {
+                    cantidadProfesional++;
+                }
+                else if (usuario.IdRol == 4)
+                {
+                    cantidadInteresado++;
+                }
+
+                totalUsuario++;
+            }
+
+            var response = new
+            {
+                totalUsuario,
+                cantidadAdmin,
+                cantidadProfesional,
+                cantidadInteresado
+            };
+
+            return Ok(response);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+}
