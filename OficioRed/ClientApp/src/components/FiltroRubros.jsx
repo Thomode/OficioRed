d602ffad36069b903c2d10928b6a3efc84bd5b68
Tomas Figueroa Lopez <91357@sistemas.frc.utn.edu.ar>
2023-11-10 19:33:12 -0300

    filtro rubros

--- OficioRed/ClientApp/src/components/FiltroRubros.jsx
+++ OficioRed/ClientApp/src/components/FiltroRubros.jsx
@@ -68,12 +68,12 @@ export const FiltroRubros = ({rubros, setRubros}) => {
           labelId="demo-multiple-checkbox-label"
           id="demo-multiple-checkbox"
           multiple
-          value={rubros
+          value={rubros && rubros
             .filter((rubro) => rubro.seleccionado)
             .map((rubro) => rubro.idRubro)}
           onChange={handleSelectChange}
           renderValue={(selected) =>
-            selected
+            selected && selected
               .map(
                 (rubroId) =>
                   rubros.find((rubro) => rubro.idRubro === rubroId).nombre
@@ -82,7 +82,7 @@ export const FiltroRubros = ({rubros, setRubros}) => {
           }
           MenuProps={MenuProps}
         >
-          {rubros.map((rubro) => (
+          {rubros && rubros.map((rubro) => (
             <MenuItem key={rubro.idRubro} value={rubro.idRubro}>
               <Checkbox checked={rubro.seleccionado} />
               <ListItemText primary={rubro.nombre} />
