97c150438d70113a3796e21e69d3579a7a05732e
tobiBordino <tobordino@gmail.com>
2023-11-13 19:26:04 -0300

    Saco id de tablas Admin

--- OficioRed/ClientApp/src/components/Rubro/TablaRubro.jsx
+++ OficioRed/ClientApp/src/components/Rubro/TablaRubro.jsx
@@ -9,23 +9,32 @@ import Paper from "@mui/material/Paper";
 import { ItemRubro } from "./ItemRubro";
 
 export function TablaRubro({ rubros, loadRubros }) {
-    return (
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead sx={{ backgroundColor: '#f0f0f0' }}>
-                    <TableRow>
-                        <TableCell align="right" sx={{ fontWeight: 'bold' }}>#</TableCell>
-                        <TableCell align="right" sx={{ fontWeight: 'bold' }}>Rubro</TableCell>
-                        <TableCell align="right" sx={{ fontWeight: 'bold' }}>Editar</TableCell>
-                        <TableCell align="right" sx={{ fontWeight: 'bold' }}>Eliminar</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rubros.map((rubro, index) => (
-                        <ItemRubro index={index} rubro={rubro} loadRubros={loadRubros}></ItemRubro>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    );
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead sx={{ backgroundColor: "#f0f0f0" }}>
+          <TableRow>
+            <TableCell align="right" sx={{ fontWeight: "bold" }}>
+              Rubro
+            </TableCell>
+            <TableCell align="right" sx={{ fontWeight: "bold" }}>
+              Editar
+            </TableCell>
+            <TableCell align="right" sx={{ fontWeight: "bold" }}>
+              Eliminar
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rubros.map((rubro, index) => (
+            <ItemRubro
+              index={index}
+              rubro={rubro}
+              loadRubros={loadRubros}
+            ></ItemRubro>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
--- OficioRed/ClientApp/src/components/Usuario/TablaUsuario.jsx
+++ OficioRed/ClientApp/src/components/Usuario/TablaUsuario.jsx
@@ -6,28 +6,39 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { ItemUsuario } from './ItemUsuario';
+import { ItemUsuario } from "./ItemUsuario";
 
 export function TablaUsuario({ usuarios, loadUsuarios }) {
-    return (
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead sx={{ backgroundColor: '#f0f0f0' }}>
-                    <TableRow>
-                    <TableCell sx={{ fontWeight: 'bold' }}>#</TableCell>
-                        <TableCell sx={{ fontWeight: 'bold' }}>Usuario</TableCell>
-                        <TableCell align="right" sx={{ fontWeight: 'bold' }}>Rol</TableCell>
-                        <TableCell align="right" sx={{ fontWeight: 'bold' }}>Fecha de Alta</TableCell>
-                        <TableCell align="right" sx={{ fontWeight: 'bold' }}>Editar</TableCell>
-                        <TableCell align="right" sx={{ fontWeight: 'bold' }}>Eliminar</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {usuarios.map((usuario, index) => (
-                        <ItemUsuario index={index} usuario={usuario} loadUsuarios={loadUsuarios}></ItemUsuario>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    );
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead sx={{ backgroundColor: "#f0f0f0" }}>
+          <TableRow>
+            <TableCell sx={{ fontWeight: "bold" }}>Usuario</TableCell>
+            <TableCell align="right" sx={{ fontWeight: "bold" }}>
+              Rol
+            </TableCell>
+            <TableCell align="right" sx={{ fontWeight: "bold" }}>
+              Fecha de Alta
+            </TableCell>
+            <TableCell align="right" sx={{ fontWeight: "bold" }}>
+              Editar
+            </TableCell>
+            <TableCell align="right" sx={{ fontWeight: "bold" }}>
+              Eliminar
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {usuarios.map((usuario, index) => (
+            <ItemUsuario
+              index={index}
+              usuario={usuario}
+              loadUsuarios={loadUsuarios}
+            ></ItemUsuario>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
