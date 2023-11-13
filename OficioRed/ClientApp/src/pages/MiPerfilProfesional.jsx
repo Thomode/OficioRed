3bf171c76819f551bc10605570787aa3b71acfc9
tobiBordino <tobordino@gmail.com>
2023-11-13 16:07:34 -0300

    Correccion de MiPerfil

--- OficioRed/ClientApp/src/pages/MiPerfilProfesional.jsx
+++ OficioRed/ClientApp/src/pages/MiPerfilProfesional.jsx
@@ -9,6 +9,7 @@ import imagenDefault from "../assets/profile.png";
 import {
   Box,
   Button,
+  Divider,
   Grid,
   IconButton,
   InputAdornment,
@@ -20,7 +21,9 @@ import VisibilityOffRoundedIcon from "@mui/icons-material/VisibilityOffRounded";
 import { LockRounded } from "@mui/icons-material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { usuarioService } from "../services/usuario.service";
+import { profesionalService } from "../services/profesional.service";
 import { FiltroRubros } from "../components/FiltroRubrosSignUp";
+import { contactoService } from "../services/contacto.service";
 
 const titleStyle = {
   fontSize: "2.5rem",
@@ -51,7 +54,25 @@ const backgroundStyle = {
 
 export const MiPerfilProfesional = () => {
   const [rubros, setRubros] = useState([]);
+  const [usuarioData, setUsuarioData] = useState({
+    nombre: "",
+    apellido: "",
+    email: "",
+    telefono: "",
+    instagram: "",
+    facebook: "",
+    descripcion: "",
+    fotoPerfil: "",
+  });
+  const [showPassword, setShowPassword] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [image, setImage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
 
   const {
     register,
@@ -60,14 +81,6 @@ export const MiPerfilProfesional = () => {
     watch,
     setValue,
   } = useForm();
-
-  const [showPassword, setShowPassword] = useState(false);
-  const togglePasswordVisibility = () => {
-    setShowPassword((prevShowPassword) => !prevShowPassword);
-  };
-
-  const [image, setImage] = useState("");
-  const [selectedFile, setSelectedFile] = useState(null);
 
   const fileSelectedHandler = (event) => {
     const file = event.target.files[0];
@@ -87,7 +100,7 @@ export const MiPerfilProfesional = () => {
   const { enqueueSnackbar } = useSnackbar();
 
   const handleClick = () => {
-    navigate(`/profesionales`);
+    navigate(-1);
   };
 
   useEffect(() => {
@@ -95,42 +108,50 @@ export const MiPerfilProfesional = () => {
       try {
         const userId = usuarioService.getId();
         const userData = await usuarioService.get(userId);
-
-        // Actualizo los campos de usuario con los datos obtenidos
-        Object.keys(userData).forEach((key) => {
-          setValue(key, userData[key]);
-        });
-
-        const interesadoData = await interesadoService.getById(userId);
-
-        // Actualizo los campos de interesado con los datos obtenidos
-        Object.keys(interesadoData).forEach((key) => {
-          setValue(key, interesadoData[key]);
-        });
-
-        if (interesadoData?.fotoPerfil) {
-          setImage(interesadoData.fotoPerfil);
-        }
-      } catch (error) {
-        console.log(
-          "Error al obtener los datos del usuario e interesado",
-          error
+        const idUser = userData.data.idUsuario;
+        const profesionalData = await profesionalService
+          .getAll()
+          .then((res) =>
+            res.find((profesional) => profesional.idUsuario === idUser)
+          );
+        const contactoData = await contactoService.getById(
+          profesionalData.idContacto
         );
+        setValue("nombre", profesionalData.nombre);
+        setValue("apellido", profesionalData.apellido);
+        setValue("email", profesionalData.email);
+        setValue("descripcion", profesionalData.descripcion);
+        setValue("fotoPerfil", profesionalData.fotoPerfil);
+        setImage(profesionalData.fotoPerfil);
+        setValue("telefono", contactoData.data.telefono);
+        setValue("instagram", contactoData.data.instagram);
+        setValue("facebook", contactoData.data.facebook);
+      } catch (error) {
+        console.log("Error al obtener los datos", error);
+        setErrorMessage("Error al obtener los datos");
       }
     };
+
     fetchData();
   }, [setValue]);
 
   const onSubmit = async (data) => {
     console.log(data);
     try {
-      await interesadoService.updateInteresado(
+      await profesionalService.updateProfesional(
         data.nombre,
         data.apellido,
         data.email,
-        selectedFile
+        data.descripcion,
+        data.fotoPerfil
       );
       await usuarioService.updateUser(data.id, data.user, data.password);
+      await contactoService.updateContacto(
+        data.id,
+        data.telefono,
+        data.instagram,
+        data.facebook
+      );
 
       navigate("/home");
       enqueueSnackbar("Actualización exitosa", {
@@ -160,7 +181,7 @@ export const MiPerfilProfesional = () => {
           container
           item
           xs={12}
-          md={6}
+          md={9}
           sm={3}
           alignItems="center"
           direction="column"
@@ -170,7 +191,7 @@ export const MiPerfilProfesional = () => {
             <Box
               display="flex"
               flexDirection={"column"}
-              maxWidth={800}
+              maxWidth={1500}
               minWidth={300}
               alignItems="center"
               justifyContent={"center"}
@@ -215,6 +236,7 @@ export const MiPerfilProfesional = () => {
                     src={image}
                     alt="Vista previa de la imagen"
                     style={imageStyle}
+                    name="fotoPerfil"
                   />
                 ) : (
                   <img
@@ -238,9 +260,8 @@ export const MiPerfilProfesional = () => {
                   />
                 </Button>
               </Grid>
-
               <Grid container spacing={2}>
-                <Grid item xs={6}>
+                <Grid item xs={12} md={4}>
                   <TextField
                     fullWidth
                     required
@@ -248,7 +269,6 @@ export const MiPerfilProfesional = () => {
                     type={"text"}
                     placeholder="Nombre"
                     autoComplete="off"
-                    label="Nombre"
                     margin="normal"
                     {...register("nombre", {
                       required: true,
@@ -276,7 +296,6 @@ export const MiPerfilProfesional = () => {
                     type={"text"}
                     placeholder="Apellido"
                     autoComplete="off"
-                    label="Apellido"
                     margin="normal"
                     {...register("apellido", {
                       required: true,
@@ -304,7 +323,6 @@ export const MiPerfilProfesional = () => {
                     type={"email"}
                     placeholder="example@email.com"
                     autoComplete="off"
-                    label="Email"
                     margin="normal"
                     {...register("email", {
                       required: true,
@@ -319,8 +337,58 @@ export const MiPerfilProfesional = () => {
                         : ""
                     }
                   />
+                  <TextField
+                    fullWidth
+                    required
+                    placeholder="Nueva Contraseña"
+                    autoComplete="off"
+                    type={showPassword ? "text" : "password"}
+                    margin="normal"
+                    label="Nueva Contraseña"
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment>
+                          <LockRounded />
+                        </InputAdornment>
+                      ),
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton
+                            onClick={togglePasswordVisibility}
+                            edge="end"
+                          >
+                            {showPassword ? (
+                              <RemoveRedEyeRoundedIcon fontSize="small" />
+                            ) : (
+                              <VisibilityOffRoundedIcon fontSize="small" />
+                            )}
+                          </IconButton>
+                        </InputAdornment>
+                      ),
+                    }}
+                    {...register("password", {
+                      required: true,
+                      minLength: 2,
+                      maxLength: 15,
+                      pattern:
+                        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$.-_,@$!%*?&])[A-Za-z\d$.-_,@$!%*?&]{4,15}$/,
+                    })}
+                    error={!!errors.password}
+                    helperText={
+                      errors.password?.type === "required"
+                        ? "Campo obligatorio"
+                        : errors.password?.type === "minLength"
+                        ? "Mínimo 2 caracteres"
+                        : errors.password?.type === "maxLength"
+                        ? "Máximo 15 caracteres"
+                        : errors.password?.type === "pattern"
+                        ? "Debe contener entre 4 y 15 caracteres y al menos una letra mayúscula, una minúscula, un número y un caracter especial"
+                        : ""
+                    }
+                  />
                 </Grid>
-                <Grid item xs={6}>
+
+                <Grid item xs={12} md={4}>
                   <TextField
                     fullWidth
                     required
@@ -328,7 +396,6 @@ export const MiPerfilProfesional = () => {
                     type="tel"
                     placeholder="Teléfono"
                     autoComplete="off"
-                    label="Teléfono"
                     margin="normal"
                     {...register("telefono", {
                       required: true,
@@ -349,7 +416,6 @@ export const MiPerfilProfesional = () => {
                     type="text"
                     placeholder="Instagram"
                     autoComplete="off"
-                    label="Instagram"
                     margin="normal"
                     {...register("instagram")}
                   />
@@ -359,19 +425,54 @@ export const MiPerfilProfesional = () => {
                     type="text"
                     placeholder="Facebook"
                     autoComplete="off"
-                    label="Facebook"
                     margin="normal"
                     {...register("facebook")}
                   />
-                </Grid>
-                <Grid item xs={12}>
-                  <FiltroRubros
-                    label="Rubros"
-                    sx={{ width: "100%" }}
-                    rubros={rubros}
-                    setRubros={setRubros}
-                    seleccionado={false}
+                  <TextField
+                    fullWidth
+                    required
+                    name="confirmPassword"
+                    placeholder="Confirmar Contraseña"
+                    autoComplete="off"
+                    type={showPassword ? "text" : "password"}
+                    label="Confirmar Contraseña"
+                    margin="normal"
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment>
+                          <LockRounded />
+                        </InputAdornment>
+                      ),
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton
+                            onClick={togglePasswordVisibility}
+                            edge="end"
+                          >
+                            {showPassword ? (
+                              <RemoveRedEyeRoundedIcon fontSize="small" />
+                            ) : (
+                              <VisibilityOffRoundedIcon fontSize="small" />
+                            )}
+                          </IconButton>
+                        </InputAdornment>
+                      ),
+                    }}
+                    {...register("confirmPassword", {
+                      required: true,
+                      validate: (value) => value === watch("password"),
+                    })}
+                    error={!!errors.confirmPassword}
+                    helperText={
+                      errors.confirmPassword?.type === "required"
+                        ? "Campo obligatorio"
+                        : errors.confirmPassword?.type === "validate"
+                        ? "Las contraseñas no coinciden"
+                        : ""
+                    }
                   />
+                </Grid>
+                <Grid item xs={12} md={4}>
                   <TextField
                     fullWidth
                     required
@@ -379,9 +480,8 @@ export const MiPerfilProfesional = () => {
                     type={"text"}
                     placeholder="Descripcion"
                     autoComplete="off"
-                    label="Descripcion"
                     multiline
-                    rows={3}
+                    rows={11}
                     margin="normal"
                     {...register("descripcion", {
                       required: true,
@@ -400,93 +500,30 @@ export const MiPerfilProfesional = () => {
                     }
                   />
                 </Grid>
+                <Grid item xs={12}>
+                  <Box mb={2}>
+                    <Divider style={{ color: "black" }} />
+
+                    <Typography
+                      variant="h6"
+                      style={{
+                        color: "#1B325F",
+                        position: "absolute",
+                        left: "53%",
+                      }}
+                    >
+                      Rubros
+                    </Typography>
+                  </Box>
+
+                  <FiltroRubros
+                    sx={{ width: "100%" }}
+                    rubros={rubros}
+                    setRubros={setRubros}
+                    seleccionado={false}
+                  />
+                </Grid>
               </Grid>
-              <TextField
-                fullWidth
-                required
-                placeholder="Contraseña"
-                autoComplete="off"
-                type={showPassword ? "text" : "password"}
-                label="Contraseña"
-                margin="normal"
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment>
-                      <LockRounded />
-                    </InputAdornment>
-                  ),
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton onClick={togglePasswordVisibility} edge="end">
-                        {showPassword ? (
-                          <RemoveRedEyeRoundedIcon fontSize="small" />
-                        ) : (
-                          <VisibilityOffRoundedIcon fontSize="small" />
-                        )}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-                {...register("password", {
-                  required: true,
-                  minLength: 2,
-                  maxLength: 15,
-                  pattern:
-                    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$.-_,@$!%*?&])[A-Za-z\d$.-_,@$!%*?&]{4,15}$/,
-                })}
-                error={!!errors.password}
-                helperText={
-                  errors.password?.type === "required"
-                    ? "Campo obligatorio"
-                    : errors.password?.type === "minLength"
-                    ? "Mínimo 2 caracteres"
-                    : errors.password?.type === "maxLength"
-                    ? "Máximo 15 caracteres"
-                    : errors.password?.type === "pattern"
-                    ? "Debe contener entre 4 y 15 caracteres y al menos una letra mayúscula, una minúscula, un número y un caracter especial"
-                    : ""
-                }
-              />
-              <TextField
-                fullWidth
-                required
-                name="confirmPassword"
-                placeholder="Confirmar Contraseña"
-                autoComplete="off"
-                type={showPassword ? "text" : "password"}
-                label="Confirmar Contraseña"
-                margin="normal"
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment>
-                      <LockRounded />
-                    </InputAdornment>
-                  ),
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton onClick={togglePasswordVisibility} edge="end">
-                        {showPassword ? (
-                          <RemoveRedEyeRoundedIcon fontSize="small" />
-                        ) : (
-                          <VisibilityOffRoundedIcon fontSize="small" />
-                        )}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-                {...register("confirmPassword", {
-                  required: true,
-                  validate: (value) => value === watch("password"),
-                })}
-                error={!!errors.confirmPassword}
-                helperText={
-                  errors.confirmPassword?.type === "required"
-                    ? "Campo obligatorio"
-                    : errors.confirmPassword?.type === "validate"
-                    ? "Las contraseñas no coinciden"
-                    : ""
-                }
-              />
 
               <Button
                 margin="normal"
@@ -495,6 +532,7 @@ export const MiPerfilProfesional = () => {
                 variant="contained"
                 color="primary"
                 fullWidth
+                marginTop={"20px"}
               >
                 Guardar cambios
               </Button>
--- OficioRed/ClientApp/src/services/contacto.service.js
+++ OficioRed/ClientApp/src/services/contacto.service.js
@@ -3,6 +3,22 @@ import axios from "axios"
 const getById = async (id) =>
     await axios.get(`/api/Contacto/${id}`)
 
+const updateContacto = async (id, telefono, email, instagram, facebook) => {
+    try {
+        const response = await axios.put(`/api/Contacto/${id}`, {
+            telefono,
+            email,
+            instagram,
+            facebook
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error al actualizar el contacto:', error);
+        throw error;
+    }
+};
+
 export const contactoService = {
-    getById
+    getById,
+    updateContacto
 }
